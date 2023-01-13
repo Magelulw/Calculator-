f1837edb6e9c0476cf779b9d1773b454622d5c49
Magelulw <magelulw@gmail.com>
2023-01-13 18:17:10 +0100

    added some functions

---
+++ main.js
@@ -0,0 +1,18 @@
+//* get numbers and signs
+
+function add(a,b){
+    return a + b
+}
+function subtract(a,b){
+    return a - b
+}
+function multiply(a,b){
+    return a * b
+}
+function divide(a,b){
+    return a / b
+}
+
+function operate(num1,num2){
+
+}
